9e410a7fdec33e2e449f2ec1202c91063bb44b3a
Doan Ho Van <doanhv@kozo-japan.com>
2022-08-20 22:16:44 +0700

    add tailwin

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.{blade.php,js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
