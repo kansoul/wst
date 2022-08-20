10a35cedc17bd2b688478e11558f79ce34515dd9
Doan Ho Van <doanhv@kozo-japan.com>
2022-08-20 21:30:25 +0700

    test to auto

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -5,29 +5,9 @@ export interface IUser {
     age: number;
 }
 const App = () => {
-    const [users, setUsers] = useState<IUser[]>([
-        {
-            name: "Bijaya",
-            age: 25,
-        },
-        {
-            name: "Ram",
-            age: 25,
-        },
-    ]);
-
     return (
         <div>
-            <h1>Users list</h1>
-            <ul>
-                {users.map((user: IUser) => {
-                    return (
-                        <li key={user.name}>
-                            {user.name} is {user.age} years old
-                        </li>
-                    );
-                })}
-            </ul>
+            <h1>First Time Auto Deploy</h1>
         </div>
     );
 };
